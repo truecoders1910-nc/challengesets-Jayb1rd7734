4bbc3415a412014e0797aa2f4eb55ad2f844ebc0
Jay Snodderly <j.smedederly@gmail.com>
2019-11-13 18:16:20 -0600

    Second two methods solved and tested,

--- ChallengeSets/ChallengeSet01.cs
+++ ChallengeSets/ChallengeSet01.cs
@@ -15,12 +15,12 @@ namespace ChallengeSets
 
         public int Add(int number1, int number2)
         {
-            throw new NotImplementedException();
+            return number1 + number2;
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            throw new NotImplementedException();
+            return number1 <= number2 ? number1 : number2;
         }
 
         public long Multiply(long factor1, long factor2)
