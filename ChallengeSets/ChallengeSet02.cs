bfc92f33328eeb9d0404535f4590280c8540b278
Jay Snodderly <j.smedederly@gmail.com>
2019-12-04 13:10:55 -0600

    Methods 6 thru 9 solved.

--- ChallengeSets/ChallengeSet02.cs
+++ ChallengeSets/ChallengeSet02.cs
@@ -27,27 +27,52 @@ namespace ChallengeSets
 
         public double SumOfMinAndMax(IEnumerable<double> numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null) return 0;
+            double max = 0, min = 5;
+            foreach (var item in numbers)
+            {
+                max = Math.Max(max, item);
+                min = Math.Min(min, item);
+            }
+            return max + min;
         }
 
         public int GetLengthOfShortestString(string str1, string str2)
         {
-            throw new NotImplementedException();
+            return str1.Length <= str2.Length ? str1.Length : str2.Length;
         }
 
         public int Sum(int[] numbers)
         {
-            throw new NotImplementedException();
+            int sum=0;
+            if (numbers == null) return 0;
+            foreach (var item in numbers)
+            {
+                sum += item;
+            }
+            return sum;
         }
 
         public int SumEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            int sum = 0;
+            if (numbers == null) return 0;
+            foreach (var item in numbers)
+            {
+               if(item % 2 == 0)sum += item;
+            }
+            return sum;
         }
 
         public bool IsSumOdd(List<int> numbers)
         {
-            throw new NotImplementedException();
+            int sum = 0;
+            if (numbers == null) return false ;
+            foreach (var item in numbers)
+            {
+                sum += item;
+            }
+            return sum % 2 == 0 ? false : true;
         }
 
         public long CountOfPositiveOddsBelowNumber(long number)
