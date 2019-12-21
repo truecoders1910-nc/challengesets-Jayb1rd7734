569527b8b043dc2b195b8ef664b24100df4a13d1
Jay Snodderly <j.smedederly@gmail.com>
2019-12-21 15:11:35 -0600

    methods 1 and 2 solved and tested.

--- ChallengeSets/ChallengeSet06.cs
+++ ChallengeSets/ChallengeSet06.cs
@@ -7,12 +7,28 @@ namespace ChallengeSets
     {
         public bool CollectionContainsWord(IEnumerable<string> words, string word, bool ignoreCase)
         {
-            throw new NotImplementedException();
+            if (words == null) return false;
+            foreach (var item in words)
+            {
+                if (item == null) continue;
+                if (ignoreCase)
+                {
+                    if (item.ToLower() == word.ToLower()) return true;
+                }
+                else if (item == word) return true;
+            }
+            return false;
         }
 
         public bool IsPrimeNumber(int num)
         {
-            throw new NotImplementedException();
+            if (num == 2) return true;
+            if (num % 2 == 0 || num < 2) return false;
+            for (int i = 2; i < num / 2; i++)
+            {
+                if (num % i == 0) return false;
+            }
+            return true;
         }
 
         public int IndexOfLastUniqueLetter(string str)
