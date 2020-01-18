cf5d70069d4d3def6cd45c211bee5d766d743bb1
Jay Snodderly <j.smedederly@gmail.com>
2020-01-17 23:05:28 -0600

    Method 7 solved and tested.

--- ChallengeSets/ChallengeSet07.cs
+++ ChallengeSets/ChallengeSet07.cs
@@ -39,13 +39,45 @@ namespace ChallengeSets
         public string GetNameOfHighestParentCompany(Business business)
         {
             // If there is Company A, whose parent is Company B, whose parent is Company C, then given Company A return Company C
-            throw new NotImplementedException();
+            if (business.ParentCompany == null) return business.Name;
+            business.Name = GetNameOfHighestParentCompany(business.ParentCompany);
+            return business.Name;
         }
 
         public enum TicTacToeResult { X, O, Draw }
         public TicTacToeResult GetTicTacToeWinner(char[,] finalBoard)
         {
-            throw new NotImplementedException();
+            string win = "100100100010010010001001001111000000000111000000000111100010001001010100";
+            string test = "";
+            char[] check;
+            for(int x = 0; x < 8; x++)
+            {
+                check = win.Substring(x * 9, 9).ToCharArray();
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        if (finalBoard[i, j] == 'X') check[i * 3 + j] = '0';
+                    }
+                }
+                test = new string(check);
+                if (test == "000000000") return TicTacToeResult.X;
+            }
+
+            for (int x = 0; x < 8; x++)
+            {
+                check = win.Substring(x * 9, 9).ToCharArray();
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        if (finalBoard[i, j] == 'O') check[i * 3 + j] = '0';
+                    }
+                }
+                test = new string(check);
+                if (test == "000000000") return TicTacToeResult.O;
+            }
+            return TicTacToeResult.Draw;
         }
 
         public bool EachArrayInJaggedArrayContainsTargetNumber(int[][] numbers, int targetNumber)
