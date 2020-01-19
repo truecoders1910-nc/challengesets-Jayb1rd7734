23a6b6aa4522580eca86834194e087cc976872e2
Jay Snodderly <j.smedederly@gmail.com>
2020-01-19 15:27:01 -0600

    Method 1 refactored and annotated.

--- ChallengeSets/ChallengeSet08.cs
+++ ChallengeSets/ChallengeSet08.cs
@@ -8,10 +8,19 @@ namespace ChallengeSets
             int index = 0;
             while (true)
             {
-                if ((hopAmount[index] + index) < 0 || hopAmount[index] == 0) return false;
-                if ((hopAmount[index] + index) > (hopAmount.Length - 1)) return true;
-                if (index + hopAmount[index] + hopAmount[index + hopAmount[index]] == index) return false;
-                index = index + hopAmount[index];
+                /*Implemented an infinite while loop. One of the following four if statements is
+                 * guaranteed to break the loop and produce a solution.
+                */
+                //
+                int value = hopAmount[index];
+                // Test for underrun                  Test for stall.
+                if ((value + index) < 0 || value == 0) return false;
+                // Test for overrun ( win ).
+                if ((value + index) > (hopAmount.Length - 1)) return true;
+                // Test for infinite loop.
+                if (index + value + hopAmount[index + value] == index) return false;
+                // Increment index by indicated amount.
+                index = index + value;
             }
         }
 
